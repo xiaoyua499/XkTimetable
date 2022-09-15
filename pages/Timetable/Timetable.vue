8fd159df7fbeda3fe4c91f0096dfa4e679d4f9ff
xya499 <302870488@qq.com>
2022-09-15 15:57:38 +0800

    完成Tbabar页面

---
+++ pages/Timetable/Timetable.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    课表
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style lang="less">
+
+</style>
---
+++ pages/results/results.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    成绩
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style lang="less">
+
+</style>
